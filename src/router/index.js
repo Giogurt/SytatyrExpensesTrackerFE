9c194aad01043941de555f4838e06645addf7cd7
Giovanni G <givanni.guzman@ilumps.com>
2022-05-04 13:40:48 -0600

    Added navbar to entire application. Added vuetify

--- src/router/index.js
+++ src/router/index.js
@@ -1,14 +1,14 @@
 import Vue from "vue";
 import VueRouter from "vue-router";
-import Home from "../views/Home.vue";
+import Expenses from "@/views/Expenses.vue";
 
 Vue.use(VueRouter);
 
 const routes = [
   {
-    path: "/",
-    name: "Home",
-    component: Home,
+    path: "/expenses",
+    name: "Expenses",
+    component: Expenses,
   },
   {
     path: "/about",
---
+++ src/views/Expenses.vue
@@ -0,0 +1,13 @@
+<template>
+  <div>
+    Soy el view de expenses
+  </div>
+</template>
+<script>
+export default {
+  name: "Expenses",
+  components: {
+  },
+};
+</script>
+<style></style>
