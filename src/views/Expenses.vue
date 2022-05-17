ce2a75513c29a4e313bd23763f83a602356d5cb9
unknown <marioalex20007@hotmail.com>
2022-05-17 01:36:58 -0500

    SET-05 Basic FE

--- src/views/Expenses.vue
+++ src/views/Expenses.vue
@@ -47,7 +47,7 @@
             </tr>
           </thead>
           <tbody>
-            <tr v-for="expense in expenses" :key="expense.id">
+            <tr v-for="(expense, index) in expenses" :key="expense.id">
               <td>{{ expense.id }}</td>
               <td>{{ expense.date }}</td>
               <td>$ {{ expense.amount }}</td>
@@ -56,7 +56,7 @@
               <td>{{ expense.reason }}</td>
               <td>{{ expense.method }}</td>
               <td>{{ expense.notes }}</td>
-              <v-btn class="ma-2" outlined small fab color="rgb(106, 140, 175)">
+              <v-btn class="ma-2" outlined small fab color="rgb(106, 140, 175)" @click="editExpense(index)">
                 <v-icon>mdi-pencil</v-icon>
               </v-btn>
               <v-btn class="ma-2" outlined small fab color="rgb(106, 140, 175)">
@@ -108,10 +108,58 @@
         <v-card-actions>
           <v-spacer></v-spacer>
           <v-col cols="12" sm="6" md="6" align="center">
-            <v-btn elevation="2" rounded color="rgb(247, 122, 114)"  @click="dialog = false">Cancel</v-btn>
+            <v-btn elevation="2" rounded color="rgb(247, 122, 114)" @click="dialog = false">Cancel</v-btn>
           </v-col>
           <v-col cols="12" sm="6" md="6" align="center">
-            <v-btn elevation="2" rounded color="rgb(238, 249, 191)"  @click="dialog = false">Save</v-btn>
+            <v-btn elevation="2" rounded color="rgb(238, 249, 191)" @click="dialog = false">Save</v-btn>
+          </v-col>
+        </v-card-actions>
+      </v-card>
+    </v-dialog>
+
+    <v-dialog v-model="dialog2" width="800" persistent>
+      <v-card>
+        <v-card-title>
+          <span class="text-h5">Modify Expense</span>
+        </v-card-title>
+        <v-card-text>
+          <v-container>
+            <v-row>
+              <v-col cols="12" sm="6" md="6">
+                <v-text-field v-model="recipient" label="Recipient" hint="The entity which is going to recieve the money" required>
+                </v-text-field>
+              </v-col>
+              <v-col cols="12" sm="6" md="6">
+                <v-text-field v-model="amount" label="Amount" hint="Amount of money expended" required></v-text-field>
+              </v-col>
+              <v-col cols="12" sm="6" md="6">
+                <v-text-field v-model="reason" label="Reason" hint="Reason to expend the money" required></v-text-field>
+              </v-col>
+              <v-col cols="12" sm="6" md="6">
+                <v-text-field v-model="notes" label="Notes" hint="Extra information" required></v-text-field>
+              </v-col>
+              <v-col cols="12" sm="6" md="6">
+                <v-select v-model="category" :items="categories" label="Category" required></v-select>
+              </v-col>
+              <v-col cols="12" sm="6" md="6">
+                <v-select v-model="method" :items="paymentMethods" label="Method" required></v-select>
+              </v-col>
+              <v-row justify="center">
+                <v-date-picker v-model="picker" color="rgb(106, 140, 175)"></v-date-picker>
+              </v-row>
+            </v-row>
+          </v-container>
+        </v-card-text>
+
+        <v-divider></v-divider>
+
+        <v-card-actions>
+          <v-spacer></v-spacer>
+          <v-col cols="12" sm="6" md="6" align="center">
+            <v-btn elevation="2" rounded color="rgb(247, 122, 114)" @click="dialog2 = false">Cancel</v-btn>
+          </v-col>
+          <v-col cols="12" sm="6" md="6" align="center">
+            <v-btn elevation="2" rounded color="rgb(238, 249, 191)" @click="dialog2 = false">Save</v-btn>
           </v-col>
         </v-card-actions>
       </v-card>
@@ -165,32 +213,32 @@ export default {
       expenses: [
         {
           id: 1,
-          date: "711",
+          date: "2020-06-04",
           amount: 100,
           recipient: "Caffenio",
-          category: "Comida",
+          category: "Food",
           reason: "Tenia hambre",
-          method: "Paypal",
+          method: "Credit",
           notes: "No lo haré de nuevo",
         },
         {
           id: 2,
-          date: "711",
+          date: "2000-08-07",
           amount: 460,
           recipient: "Caffenio",
-          category: "Basketball",
+          category: "Insurance",
           reason: "Tenia hambre",
-          method: "Paypal",
+          method: "Debit",
           notes: "No lo haré de nuevo",
         },
         {
           id: 3,
-          date: "711",
+          date: "2000-10-02",
           amount: 250,
           recipient: "Caffenio",
-          category: "Football",
+          category: "Housing",
           reason: "Tenia hambre",
-          method: "Paypal",
+          method: "Cash",
           notes: "No lo haré de nuevo",
         },
       ],
@@ -201,9 +249,13 @@ export default {
       lowestExpense: '',
       totalExpenses: '',
       dialog: false,
+      dialog2: false,
       picker: (new Date(Date.now() - (new Date()).getTimezoneOffset() * 60000)).toISOString().substr(0, 10),
-      categories: ['Housing', 'Food', 'Clothing', 'Peronal Care', 'Automobile', 'Property Tax', 'Utilities', 'Entertainment', 'Unreimursed Business Expenses', 'Alimony (paid)', 'Child Support (paid)', 'Childrens Expenses', 'Gifts', 'Charitable Contributions', 'Medical Expenses', 'Insurance', 'Credit cards', 'Other Liabilities'],
-      paymentMethods: ['Credit', 'Debit', 'Cash', 'Check', 'Transfer']
+      categories: ['Housing', 'Food', 'Clothing', 'Peronal Care', 'Automobile', 'Property Tax', 'Utilities',
+      'Entertainment', 'Unreimursed Business Expenses', 'Alimony (paid)', 'Child Support (paid)', 'Childrens Expenses',
+      'Gifts', 'Charitable Contributions', 'Medical Expenses', 'Insurance', 'Credit cards', 'Other Liabilities'],
+      paymentMethods: ['Credit', 'Debit', 'Cash', 'Check', 'Transfer'],
+      idForEdition: ''
     }
   },
 
@@ -237,6 +289,17 @@ export default {
 
       this.totalExpenses = temp;
 
+    },
+
+    editExpense(index){
+      this.dialog2 = true;
+      this.recipient = this.expenses[index].recipient;
+      this.amount = this.expenses[index].amount;
+      this.reason= this.expenses[index].reason;
+      this.notes = this.expenses[index].notes;
+      this.category = this.expenses[index].category;
+      this.method = this.expenses[index].method;
+      this.picker = this.expenses[index].date;
     }
   },
 
